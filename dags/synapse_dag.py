910014bd7a59fe1f4caaf541795a7b5dfec257e4
ambika-garg <70703123+ambika-garg@users.noreply.github.com>
2023-10-11 08:09:25 -0400

    created simple dag

---
+++ dags/synapse_dag.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="AzureSynapseRunPipelineDag",
+    schedule=None,
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["pipeline"],
+) as dag:
+    python_version = BashOperator(task_id="pythonversion", bash_command="python --version")
+    python_version

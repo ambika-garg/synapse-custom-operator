11d14b010703e33a93e0273e334c8ce7cb495e9b
ambika-garg <ambikagarg1101@gmail.com>
2023-11-16 13:40:51 -0500

    Fix imports

--- plugins/operators/RunSynapsePipelineOperator.py
+++ plugins/operators/RunSynapsePipelineOperator.py
@@ -7,7 +7,11 @@ from urllib.parse import urlencode
 from airflow.exceptions import AirflowException
 from airflow.hooks.base import BaseHook
 from airflow.models import BaseOperator, BaseOperatorLink, XCom
-from hooks.azureSynapseHook import AzureSynapsePipelineHook, AzureSynapsePipelineRunException, AzureSynapsePipelineRunStatus
+from hooks.azureSynapseHook import (
+    AzureSynapsePipelineHook,
+    AzureSynapsePipelineRunException,
+    AzureSynapsePipelineRunStatus
+)
 
 
 if TYPE_CHECKING:

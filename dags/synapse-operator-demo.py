8f5ccf4bf26d9f964702abb1e294d07b69042a6c
ambika-garg <ambikagarg1101@gmail.com>
2023-11-16 12:34:53 -0500

    Add Synapse operator for demo in Managed Airflow

---
+++ dags/synapse-operator-demo.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from airflow.models import DAG
+from airflow.providers.microsoft.azure.operators.synapse import AzureSynapseRunPipelineOperator
+
+
+with DAG(
+    dag_id="example_synapse_run_pipeline",
+    start_date=datetime(2021, 8, 13),
+    schedule="@daily",
+    catchup=False,
+    tags=["synapse", "example"],
+) as dag:
+    
+    run_pipeline1 = AzureSynapseRunPipelineOperator(
+        task_id="run_pipeline1",
+        azure_synapse_conn_id="azure_synapse_connection",
+        pipeline_name="Pipeline 1",
+        azure_synapse_workspace_dev_endpoint="https://ambika-synapse-workspace.dev.azuresynapse.net",
+    )
+
+    run_pipeline1

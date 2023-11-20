8ea13df92e7adb4d64e87d5fd143a47192788fc6
Ambika Garg <v-ambikagarg@microsoft.com>
2023-11-20 12:41:08 -0500

    Added Airflow Plugin

---
+++ plugins/sample_plugin.py
@@ -0,0 +1,14 @@
+from airflow.plugins_manager import AirflowPlugin
+from hooks.azureSynapseHook import AzureSynapsePipelineHook
+from operators.RunSynapsePipelineOperator import AzureSynapsePipelineRunLink, AzureSynapseRunPipelineOperator
+
+
+# Defining the plugin class
+class AirflowTestPlugin(AirflowPlugin):
+    name = "test_plugin"
+    # hooks = [AzureSynapsePipelineHook]
+    operator_extra_links = [
+        AzureSynapsePipelineRunLink(),
+     ]
+    # extra_links = [AzureSynapsePipelineRunLink]
+    # operators = [AzureSynapseRunPipelineOperator]

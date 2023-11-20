84903bc9174826ff77a680daf5a2c578495850bc
Ambika Garg <v-ambikagarg@microsoft.com>
2023-11-20 13:08:39 -0500

    Comment out unused files

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,16 +1,16 @@
-__version__ = "1.0.0"
+# __version__ = "1.0.0"
 
-def get_provider_info():
-    return {
-        "package-name": "airflow-providers-local-sample",
-        "name": "Microsoft Azure",
-        "description": "Synapse Operator run locally.",
-        "connection-types": [
-            {
-                "connection-type": "azure_synapse_pipeline",
-                "hook-class-name": "plugins.hooks.azureSynapseHook.AzureSynapsePipelineHook"
-            }
-        ],
-        "extra-links": ["plugins.operators.RunSynapsePipelineOperator.AzureSynapsePipelineRunLink"],
-        "versions": [__version__],  # Required
-    }
+# def get_provider_info():
+#     return {
+#         "package-name": "airflow-providers-local-sample",
+#         "name": "Microsoft Azure",
+#         "description": "Synapse Operator run locally.",
+#         "connection-types": [
+#             {
+#                 "connection-type": "azure_synapse_pipeline",
+#                 "hook-class-name": "plugins.hooks.azureSynapseHook.AzureSynapsePipelineHook"
+#             }
+#         ],
+#         "extra-links": ["plugins.operators.RunSynapsePipelineOperator.AzureSynapsePipelineRunLink"],
+#         "versions": [__version__],  # Required
+#     }
